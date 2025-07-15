b591893fe59c6a59605c6e938e17b78f9d51f0d6
Courts <courtneycook2006@gmail.com>
2025-07-15 22:20:21 +0200

    Update Login.js

--- chat-frontend/src/Login.js
+++ chat-frontend/src/Login.js
@@ -10,7 +10,7 @@ const Login = ({ setToken }) => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://codeconnect-3r9w.onrender.com/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
