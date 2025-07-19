f0f8d5f42cd5f7b7360673a850d553c96b86fc9c
Courts <courtneycook2006@gmail.com>
2025-07-19 10:43:50 +0200

    Update Signup.js

--- chat-frontend/src/Signup.js
+++ chat-frontend/src/Signup.js
@@ -15,7 +15,7 @@ const Signup = () => {
     }
 
     try {
-      const response = await fetch("https://connecthub-9k2z.onrender.com/api/signup", {
+      const response = await fetch("https://connecthub-nd7o.onrender.com/api/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
